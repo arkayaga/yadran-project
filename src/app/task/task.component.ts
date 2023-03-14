2e210760dda64da56ae08dd1df5efe55e5528c16
arkayaga <arkayagabaloglu@gmail.com>
2023-03-14 17:25:06 +0300

    form array

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent {
+  orderForm: FormGroup;
+  items: FormArray;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.orderForm = new FormGroup({
+      items: new FormArray([])
+    });
+  }
+
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+      text1: [null, [Validators.required]],
+      text2: [null],
+      select: [null, [Validators.required]]
+    });
+  }
+
+  onAdd() {
+    this.items = this.orderForm.get('items') as FormArray;
+    if (this.orderForm.valid) {
+      this.items.push(this.createItem());
+    }
+  }
+
+  onDelete() {
+    this.items = this.orderForm.get('items') as FormArray;
+    this.items.clear();
+  }
 }
