4137eccf9539696cdbc1b6c8b2d3b17f4afbf25c
arkayaga <arkayagabaloglu@gmail.com>
2023-04-25 14:40:23 +0300

    apex donut added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,26 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
+import { OrganizationService } from '../core/organization/organization.service';
 
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart,
+  ApexPlotOptions,
+  ChartComponent,
+  ApexDataLabels
+} from "ng-apexcharts";
+import * as moment from 'moment';
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  plotOptions: ApexPlotOptions;
+  labels: any;
+  dataLabels: ApexDataLabels;
+};
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,7 +30,58 @@ export class HomeComponent implements OnInit, OnDestroy {
   username: string;
   private userSub: Subscription;
 
-  constructor(private authService: AuthService) { }
+  organizationStatusId: string;
+  beginDate: string;
+  endDate: string;
+
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+  constructor(private authService: AuthService,
+    private organizationService: OrganizationService) {
+    this.getChartData();
+    this.chartOptions = {
+      series: [],
+      chart: {
+        type: "donut",
+        width: '100%',
+        height: 'auto'
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            labels: {
+              show: true,
+              total: {
+                showAlways: false,
+                show: true,
+              }
+            }
+          }
+        }
+      },
+      labels: [],
+      // responsive: [
+      //   {
+      //     breakpoint: 480,
+      //     options: {
+      //       chart: {
+      //         width: 40
+      //       },
+      //       legend: {
+      //         position: "bottom"
+      //       }
+      //     }
+      //   }
+      // ],
+      dataLabels: {
+        enabled: true,
+        formatter(_value, { seriesIndex, w }) {
+          return w.config.series[seriesIndex]
+        }
+      }
+    };
+  }
 
   ngOnInit(): void {
     this.userSub = this.authService.getUser().subscribe(user => {
@@ -22,6 +92,21 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.userSub.unsubscribe();
+  }
+
+  getChartData() {
+    const request = {
+      organizationStatusId: this.organizationStatusId,
+      beginDate: this.beginDate ? moment(this.beginDate) : null,
+      endDate: this.endDate ? moment(this.endDate) : null,
+    };
+    this.organizationService.getChartData(request).subscribe((x: any) => {
+      x.data.forEach(y => {
+        this.chartOptions.series.push(y.count)
+        this.chartOptions.labels.push(y.placeName)
+
+      });
+    })
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
 
 
 @NgModule({
@@ -9,8 +11,11 @@ import { HomeComponent } from "./home.component";
     imports: [
         CommonModule,
         HomeRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        SharedModule,
     ],
 
 })
 
-export class HomeModule {}
+export class HomeModule { }
